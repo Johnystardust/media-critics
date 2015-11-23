b9355d782e0e642962c9223cbd81fe109522cb76
Tim van der Slik Laptop <timvdslik@gmail.com>
2015-11-23 10:36:33 +0100

    Implemented slider

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,7 +4,11 @@
 
 $(document).ready(function(){
 
-    // Show the work info
+    /*
+     |----------------------------------------------------------------
+     |   Show the work info
+     |----------------------------------------------------------------
+     */
     $('.work-block').click(function(){
         // Get the data number to know the number of block
         var number = $(this).attr('data-number');
@@ -30,7 +34,11 @@ $(document).ready(function(){
         return false;
     });
 
-    // Close the work-info
+    /*
+     |----------------------------------------------------------------
+     |   Close the work-info
+     |----------------------------------------------------------------
+     */
     $('.close-button').click(function(){
         // Set the work info back to original
         $('.active').find('.work-info').css('left', '300%');
@@ -44,20 +52,134 @@ $(document).ready(function(){
         return false;
     });
 
-
+    /*
+     |----------------------------------------------------------------
+     |   Hover Function
+     |----------------------------------------------------------------
+     */
     $('.strategy-point').hover(function(){
         $(this).find('.icon-wrapper').addClass('animated flip');
-    });
-
-    $('.strategy-point').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    }, function(){
         $(this).find('.icon-wrapper').removeClass('animated flip');
     });
 
+    //$('.strategy-point').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    //    $(this).find('.icon-wrapper').removeClass('animated flip');
+    //});
+
     $('.service').hover(function(){
         $(this).find('.small-icon-wrapper').addClass('animated flip');
-    });
-
-    $('.service').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    }, function(){
         $(this).find('.small-icon-wrapper').removeClass('animated flip');
     });
+
+    //$('.service').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    //    $(this).find('.small-icon-wrapper').removeClass('animated flip');
+    //});
+
+
+    /*
+     |----------------------------------------------------------------
+     |  Slider Function
+     |
+     |  This is the function for te main slider
+     |----------------------------------------------------------------
+    */
+
+    // Get some variables we can work with
+    var ul = $('#slide-container');
+    var slide_count = ul.children().length;
+
+    var ul_width = (slide_count * 100) + "%";
+    var slide_width = (100 / slide_count) + "%";
+
+    var slide_index = 0;
+
+    // Set the container width to slide * 100%
+    ul.css('width', ul_width);
+
+    // Set the slide width to 100% / slide count
+    ul.find('li').each(function(i){
+        var left_precent = (slide_width * i) + "%";
+
+        $(this).css('left', left_precent);
+        $(this).css('width', slide_width)
+    });
+
+
+    /*
+     |----------------------------------------------------------------
+     |  Timer Function
+     |----------------------------------------------------------------
+     */
+    var timer;
+
+    function slide_timer(){
+        if(slide_index <= (slide_count - 2)){
+            slide(slide_index + 1);
+        }
+        else {
+            slide(0);
+            slide_index = 0;
+        }
+    }
+    timer = setInterval(slide_timer, 7000);
+
+    /*
+     |----------------------------------------------------------------
+     |  Menu Functions
+     |----------------------------------------------------------------
+    */
+    // Listener for the prev button
+    $('.slide-left').click(function(){
+        // Clear the timer
+        clearInterval(timer);
+        timer = setInterval(slide_timer, 7000);
+
+        slide(slide_index - 1);
+    });
+
+    // Listener for the next button
+    $('.slide-right').click(function(){
+        // Clear the timer
+        clearInterval(timer);
+        timer = setInterval(slide_timer, 7000);
+
+        slide(slide_index + 1);
+    });
+
+    /*
+     |----------------------------------------------------------------
+     |  Slide Functions
+     |----------------------------------------------------------------
+    */
+    function slide(new_slide_index){
+        // Show the prev button only when there is a prev slide
+        if(new_slide_index < 1){
+            $('.slide-left').addClass('animated bounceOutLeft');
+        }
+        else {
+            $('.slide-left').addClass('bounceInLeft');
+            $('.slide-left').removeClass('bounceOutLeft');
+        }
+
+        // Show the next button only when there is a next slide
+        if(new_slide_index >= (slide_count - 1)){
+            $('.slide-right').addClass('animated bounceOutRight');
+        }
+        else {
+            $('.slide-right').addClass('bounceInRight');
+            $('.slide-right').removeClass('bounceOutRight');
+        }
+
+        // Set the animate distance
+        var animate_left = (new_slide_index * (-100)) + "%";
+
+        // Apply the animate distance
+        ul.css('margin-left', animate_left);
+
+        // The slide_index is the new_slide_index
+        slide_index = new_slide_index;
+    }
+
 });
--- includes/theme-files/slider.php
+++ includes/theme-files/slider.php
@@ -23,7 +23,7 @@
         <div class="slide-points"></div>
     </div>
 
-    <ul class="slide-container">
+    <ul id="slide-container">
 
         <li class="slide">
             <div class="overlay">
@@ -51,6 +51,63 @@
                 </a>
             </div>
             <img src="assets/images/photo2.jpg" alt="" width="100%"/>
+        </li>
+
+        <li class="slide">
+            <div class="overlay">
+                <span class="slide-title"><h2 class="no-margin">Media Critics</h2></span><br/>
+                <span class="slide-text">Welcome on the website of media critics, please feel free to browse and contact us if any questions occur.</span>
+                <a href="#">
+                    <span class="slide-link">
+                        <i class="icon-right-open icon"></i>
+                        <span class="bg"></span>
+                    </span>
+                </a>
+            </div>
+            <img src="assets/images/photo3.jpg" alt="" width="100%"/>
+        </li>
+
+
+        <li class="slide">
+            <div class="overlay">
+                <span class="slide-title"><h2 class="no-margin">Media Critics</h2></span><br/>
+                <span class="slide-text">Welcome on the website of media critics, please feel free to browse and contact us if any questions occur.</span>
+                <a href="#">
+                    <span class="slide-link">
+                        <i class="icon-right-open icon"></i>
+                        <span class="bg"></span>
+                    </span>
+                </a>
+            </div>
+            <img src="assets/images/photo4.jpg" alt="" width="100%"/>
+        </li>
+
+        <li class="slide">
+            <div class="overlay">
+                <span class="slide-title"><h2 class="no-margin">Media Critics</h2></span><br/>
+                <span class="slide-text">Welcome on the website of media critics, please feel free to browse and contact us if any questions occur.</span>
+                <a href="#">
+                    <span class="slide-link">
+                        <i class="icon-right-open icon"></i>
+                        <span class="bg"></span>
+                    </span>
+                </a>
+            </div>
+            <img src="assets/images/photo5.jpg" alt="" width="100%"/>
+        </li>
+
+        <li class="slide">
+            <div class="overlay">
+                <span class="slide-title"><h2 class="no-margin">Media Critics</h2></span><br/>
+                <span class="slide-text">Welcome on the website of media critics, please feel free to browse and contact us if any questions occur.</span>
+                <a href="#">
+                    <span class="slide-link">
+                        <i class="icon-right-open icon"></i>
+                        <span class="bg"></span>
+                    </span>
+                </a>
+            </div>
+            <img src="assets/images/photo6.jpg" alt="" width="100%"/>
         </li>
 
 
